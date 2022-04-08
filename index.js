b5186d1106a501baa157f62399cf11459ee779fe
Mariamuwane <haroonmareeam3@gmail.com>
2022-04-08 20:34:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+const sections = document.querySelectorAll('.section')
+const controls = document.querySelectorAll('.controls')
+
+// buttons
+const control = document.querySelectorAll('.control')
+const allBody = document.querySelectorAll('.full-content')
+
