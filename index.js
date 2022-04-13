88d33700acb08d7626836b66e570a47dd604f9fe
Mariamuwane <haroonmareeam3@gmail.com>
2022-04-13 15:57:03 +0100

    some errors updated

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ function darkMode() {
 
 
 //Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+const mybutton = document.getElementById("myBtn");
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+window.scroll(scrollFunction())
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -33,6 +35,15 @@ var lightText = function() {
 }
 lightMode.addEventListener("click", lightText)
 lightText;
+
+// var lightText = function() {
+//   if (document.lightMode.textContent = "Dark mode"){
+//     document.getElementById("darkButton").textContent = "Light mode"
+//   }
+//   else{
+//     document.getElementById("darkButton").textContent = "Dark mode"
+//   }
+// }
 
 const sections = document.querySelectorAll('.section')
 const controls = document.querySelectorAll('.controls')
